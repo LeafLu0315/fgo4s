1f2e2cbfc9131e22b8d12616cd67eeb8ddb1dc2a
leaflu0315 <johnson7511333@gmail.com>
2023-08-01 12:20:26 +0800

    台 22'萬聖

--- units.js
+++ units.js
@@ -54,8 +54,8 @@ var AllCategoryNUM = {
 	servents['rider'].length, servents['caster'].length, servents['assassin'].length, servents['berserker'].length,
 	servents['ruler'].length, servents['avenger'].length, servents['alterego'].length, servents['foreigner'].length, servents['mooncancer'].length, servents['pretender'].length],
 	// 台服
-	"tw": [22, 19, 20,
-		13, 15, 15, 14,
+	"tw": [22, 20, 20,
+		14, 15, 15, 14,
 		4, 2, 3, 2, 1, 0],
 	// 自選
 	"z":  [14, 9, 12, 5, 8, 8, 8, 1, 2, 0, 0, 0, 1]
