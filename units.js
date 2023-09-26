dc887c944f1b446408ede7d7523ce59a1b3a9d65
leaflu0315 <johnson7511333@gmail.com>
2023-09-26 20:31:26 +0800

    台 Making Christmas Party

--- units.js
+++ units.js
@@ -55,7 +55,7 @@ var AllCategoryNUM = {
 	servents['ruler'].length, servents['avenger'].length, servents['alterego'].length, servents['foreigner'].length, servents['mooncancer'].length, servents['pretender'].length],
 	// 台服
 	"tw": [22, 20, 20,
-		14, 15, 15, 14,
+		14, 16, 15, 14,
 		4, 3, 3, 2, 1, 0],
 	// 自選
 	"z":  [14, 9, 12, 5, 8, 8, 8, 1, 2, 0, 0, 0, 1]
