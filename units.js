b25ff93a1b10288ad172819c8b9c8675ccc46a98
leaflu0315 <johnson7511333@gmail.com>
2024-08-28 19:22:34 +0800

    奏章III

--- units.js
+++ units.js
@@ -28,8 +28,8 @@ var servents = {
 	'ruler': [135, 233, 242, 320, 364],
 	'avenger': [147, 158, 328, 356, 388, 420],
 	'alterego': [164, 190, 191, 338],
-	'foreigner': [222, 308, 389],
-	'mooncancer': [166],
+	'foreigner': [222, 308, 389, 423],
+	'mooncancer': [166, 422],
 	'pretender': [335, 367, 372, 392, 410]
 }
 
