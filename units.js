64146064f00e035b4243cfb451e7994288a436d0
leaflu0315 <johnson7511333@gmail.com>
2025-01-14 16:21:14 +0800

    台 AC合作

--- units.js
+++ units.js
@@ -55,7 +55,7 @@ var AllCategoryNUM = {
 	servents['ruler'].length, servents['avenger'].length, servents['alterego'].length, servents['foreigner'].length, servents['mooncancer'].length, servents['pretender'].length],
 	// 台服
 	"tw": [25, 20, 21,
-		16, 18, 18, 15,
+		16, 18, 19, 15,
 		5, 4, 4, 2, 1, 3],
 	// 自選
 	"z":  [14, 9, 12, 5, 8, 8, 8, 1, 2, 0, 0, 0, 1],
